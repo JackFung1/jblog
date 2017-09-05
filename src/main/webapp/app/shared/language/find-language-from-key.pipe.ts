d19234b69dd1eb67cdd491c613523024adb03a0a
jitao.feng <jitao.feng@cienet.com.cn>
2017-09-05 11:27:18 +0800

    exe jhipster languages to support zh-tw international

--- src/main/webapp/app/shared/language/find-language-from-key.pipe.ts
+++ src/main/webapp/app/shared/language/find-language-from-key.pipe.ts
@@ -4,6 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FindLanguageFromKeyPipe implements PipeTransform {
     private languages: any = {
         'zh-cn': { name: '中文（简体）' },
+        'zh-tw': { name: '繁體中文' },
         'en': { name: 'English' }
         // jhipster-needle-i18n-language-key-pipe - JHipster will add/remove languages in this object
     };
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -101,7 +101,8 @@ module.exports = (options) => {
                 output: {
                     groupBy: [
                         { pattern: "./src/main/webapp/i18n/en/*.json", fileName: "./i18n/en.json" },
-                        { pattern: "./src/main/webapp/i18n/zh-cn/*.json", fileName: "./i18n/zh-cn.json" }
+                        { pattern: "./src/main/webapp/i18n/zh-cn/*.json", fileName: "./i18n/zh-cn.json" },
+                        { pattern: "./src/main/webapp/i18n/zh-tw/*.json", fileName: "./i18n/zh-tw.json" }
                         // jhipster-needle-i18n-language-webpack - JHipster will add/remove languages in this array
                     ]
                 }
